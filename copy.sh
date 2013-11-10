19a38614f54149080ed3a496db552521f9476c5f
Masataro Asai <guicho2.71828@gmail.com>
2013-11-10 23:18:51 +0900

    update the mathjax directory

--- copy.sh
+++ copy.sh
@@ -2,4 +2,12 @@
 
 d=$(dirname $0)
 
-ls -1 $d | while read line ; do cp -ru $d/$line ./ ; done
+ls -1 $d | while read line
+do
+    cp -ru $d/$line ./
+done
+
+if [ ! -a ./presen.org ]
+then
+    sed -e "s|@HOME|$HOME|g" $d/presen.org > ./presen.org
+fi
