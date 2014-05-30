c452573172471a5c07d6e6aa232f58cd5d062794
Masataro Asai <guicho2.71828@gmail.com>
2014-05-30 15:09:54 +0900

    added org-table

--- compile-org-html.el
+++ compile-org-html.el
@@ -3,6 +3,7 @@
 
 (defun compile-org (in out)
   (require 'ox-html)
+  (require 'org-table)
   (find-file in)
   (org-html-export-as-html nil nil nil nil)
   (write-file out))
--- compile-org-latex.el
+++ compile-org-latex.el
@@ -2,6 +2,7 @@
 (add-to-list 'load-path (concat default-directory "org-mode/lisp/"))
 (defun compile-org (in out)
   (require 'ox-latex)
+  (require 'org-table)
   (find-file in)
   (org-latex-export-as-latex nil nil nil t)
   (write-file out))
