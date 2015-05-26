cb7155cc7b19996ce46b931e88ac6491e5beb310
Masataro Asai <guicho2.71828@gmail.com>
2015-05-27 03:44:38 +0900

    defaultimagewidth

--- scripts/compile-org-latex.el
+++ scripts/compile-org-latex.el
@@ -4,5 +4,8 @@
   (require 'ox-latex)
   (require 'org-table)
   (find-file in)
+  (setf org-latex-image-default-width "\\defaultimagewidth"
+        ;; org-latex-image-default-height "\\defaultimageheight"
+        )
   (org-latex-export-as-latex nil nil nil t)
   (write-file out))
