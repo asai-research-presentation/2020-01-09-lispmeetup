c875644f8cec129743ae8ac2b2338edb82689afd
Masataro Asai <guicho2.71828@gmail.com>
2016-06-12 15:02:05 +0100

    update deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,21 @@
 #!/bin/bash -x
 
-[ -d _deploy ] || git clone . _deploy
+remote=$1
+head=$(git rev-parse HEAD)
+
+make
+git clone . _deploy
+rsync -r --exclude=.git --exclude=MathJax --exclude=org-mode --exclude=htmlize . _deploy
+
+# trap "cd $(pwd); rm -r _deploy" EXIT
+# trap "git reset $head" EXIT
+# trap "popd; rm -r _deploy" INT
 
 pushd _deploy
-git pull
-
-head=$(git rev-parse HEAD)
-trap "git reset $head" EXIT
-trap "popd; rm -r _deploy" INT
 
 git checkout --detach
-make
 git add -f --all
-git reset HEAD -- org-mode htmlize
 git commit -q -m "Deployed $(date)"
-git tag deploy-$(date +%Y-%m-%d-%H-%M)
-git push -f $1 HEAD:refs/heads/gh-pages
+git tag deploy-$(date +%Y%m%d%H%M)
+git push -f $remote HEAD:refs/heads/gh-pages
 
