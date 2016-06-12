8c4a1a527cad873a67e76b2e6bc3dfca25e25f6a
Masataro Asai <guicho2.71828@gmail.com>
2016-06-12 15:15:02 +0100

    print name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,3 +19,6 @@ git commit -q -m "Deployed $(date)"
 git tag deploy-$(date +%Y%m%d%H%M)
 git push -f $remote HEAD:refs/heads/gh-pages
 
+# git@github.com:user/repo
+
+echo $remote | sed 's_[:/]_ _g' | awk '{print "https://"$2".github.io/"$3}'
