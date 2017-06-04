a377d1f68de570a7b2488dfb12f3884133ec44f1
Masataro Asai <guicho2.71828@gmail.com>
2017-06-04 21:27:05 +0900

    [cherry-pick] set up minted

--- latexmk/rc_ja.pl
+++ latexmk/rc_ja.pl
@@ -1,6 +1,6 @@
 # -*- cperl -*-
 # latexmkrc
 
-$latex         = 'platex';
+$latex         = 'platex -shell-escape';
 $bibtex        = 'pbibtex';
 $dvipdf = "dvipdfm %O -f latexmk/ipa.map -o %D %S";
--- scripts/compile-org-latex.el
+++ scripts/compile-org-latex.el
@@ -6,7 +6,13 @@
   (find-file in)
   (setf org-latex-image-default-width ""
         ;; org-latex-image-default-height "\\defaultimageheight"
-        )
+        org-latex-listings 'minted
+        org-latex-minted-langs '((lisp "common-lisp")
+                                 (emacs-lisp "common-lisp")
+                                 (cc "c++")
+                                 (cperl "perl")
+                                 (shell-script "bash")
+                                 (caml "ocaml")))
   ;; (setf org-latex-image-default-width "\\defaultimagewidth"
   ;;       ;; org-latex-image-default-height "\\defaultimageheight"
   ;;       )
