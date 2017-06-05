e11900dba22763bcb5269b7e08ef40cb57a1d370
Masataro Asai <guicho2.71828@gmail.com>
2017-06-05 19:58:19 +0900

    [cherry-pick] MathJax deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,13 @@ head=$(git rev-parse HEAD)
 
 make
 git clone . _deploy
-rsync -r --exclude=.git --exclude=org-mode --exclude=htmlize --exclude=_deploy . _deploy
+rsync -r --exclude=.git --exclude=org-mode \
+      --exclude=htmlize --exclude=_deploy \
+      --exclude=MathJax/fonts \
+      --exclude=MathJax/unpacked \
+      --exclude=MathJax/jax/output/SVG \
+      --exclude=MathJax/jax/output/HTML-CSS \
+      . _deploy
 
 # trap "cd $(pwd); rm -r _deploy" EXIT
 # trap "git reset $head" EXIT
@@ -15,6 +21,7 @@ pushd _deploy
 
 git checkout --detach
 git add -f --all
+git add -f MathJax
 git commit -q -m "Deployed $(date)"
 git tag deploy-$(date +%Y%m%d%H%M)
 git push -f $remote HEAD:refs/heads/gh-pages
