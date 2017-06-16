c2a1e9ffddcf8a257bf0cccbb6ae4b3aa3c0c0aa
Masataro Asai <guicho2.71828@gmail.com>
2017-06-16 11:10:38 -0400

    fix mathjaxconf for both offline/online

--- js/mathjaxconf.js
+++ js/mathjaxconf.js
@@ -1,10 +1,11 @@
 MathJax.Hub.Config({
     // Only one of the two following lines, depending on user settings
     // First allows browser-native MathML display, second forces HTML/CSS
-    config: ["MMLorHTML.js"],
-    // jax: ["input/TeX"],
-    //  jax: ["input/TeX", "output/HTML-CSS"],
-    jax: ["input/TeX","output/NativeMML"],
+    // config: ["Safe.js"],
+    jax: ["input/TeX","output/CommonHTML"],
+    // config: ["MMLorHTML.js"], jax: ["input/TeX"],
+     // jax: ["input/TeX", "output/HTML-CSS"],
+    // jax: ["input/TeX","output/CommonMML"],
     extensions: ["tex2jax.js","TeX/AMSmath.js","TeX/AMSsymbols.js",
                  "TeX/noUndefined.js"],
     tex2jax: {
